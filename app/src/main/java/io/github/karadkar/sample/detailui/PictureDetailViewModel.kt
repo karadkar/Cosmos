ec6f33ce95af434b07d0111d3721e3211a62050e
Rohit Karadkar <karadkar.rohit@gmail.com>
2021-04-08 15:22:52 +0530

    detail-screen: removed un-used code

--- app/src/main/java/io/github/karadkar/sample/detailui/PictureDetailViewModel.kt
+++ app/src/main/java/io/github/karadkar/sample/detailui/PictureDetailViewModel.kt
@@ -14,8 +14,6 @@ import io.reactivex.subjects.PublishSubject
 class PictureDetailViewModel(
     private val repository: NasaImageRepository
 ) : ViewModel() {
-    private val pictureDetails = LinkedHashMap<String, PictureDetail>()
-    private val imageIds = mutableListOf<String>()
 
     private val eventEmitter = PublishSubject.create<PictureDetailViewEvent>()
     private lateinit var disposable: Disposable
@@ -23,17 +21,6 @@ class PictureDetailViewModel(
     val viewState: Observable<PictureDetailViewState>
 
     init {
-        repository.getImages().values.forEach { value ->
-            pictureDetails[value.id] = PictureDetail(
-                id = value.id,
-                imageUrl = value.imageUrlHd,
-                title = value.title,
-                author = value.copyright,
-                date = value.date!!,
-                description = value.explanation
-            )
-            imageIds.add(value.id)
-        }
 
         eventEmitter
             .doOnNext { logInfo("---> event: $it") }
@@ -55,12 +42,6 @@ class PictureDetailViewModel(
     fun submitEvent(event: PictureDetailViewEvent) {
         eventEmitter.onNext(event)
     }
-
-    fun getPictureDetail(imageId: String): PictureDetail = pictureDetails[imageId]!!
-//    fun getPictureDetail(indexPosition: Int): PictureDetail = pictureDetails[imageIds[indexPosition]]!!
-//    fun getTotalCount(): Int = pictureDetails.size
-//    fun getTotalImageIds(): List<String> = imageIds
-//    fun getDefaultPagePosition(defaultImageId: String): Int = imageIds.indexOf(defaultImageId)
 
     private fun Observable<PictureDetailViewEvent>.eventToResult(): Observable<out PictureDetailEventResult> {
         return publish { o ->
