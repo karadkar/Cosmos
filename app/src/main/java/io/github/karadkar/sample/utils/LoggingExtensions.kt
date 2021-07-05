8309a23c6408cd35de4faecd2b2f1311854cc769
Rohit Karadkar <karadkar.rohit@gmail.com>
2021-07-05 10:18:43 +0530

    fix empty tag message skips entire leg message

--- app/src/main/java/io/github/karadkar/sample/utils/LoggingExtensions.kt
+++ app/src/main/java/io/github/karadkar/sample/utils/LoggingExtensions.kt
@@ -2,10 +2,12 @@ package io.github.karadkar.sample.utils
 
 import android.util.Log
 
-fun Any.logError(message: String, t: Throwable? = null) {
-    Log.e(this.javaClass.simpleName, message, t)
+fun Any.logError(message: String, t: Throwable? = null, tag: String = this.javaClass.simpleName) {
+    val _tag = if (tag.isEmpty()) "Unknown" else tag
+    Log.e(_tag, message, t)
 }
 
-fun Any.logInfo(message: String) {
-    Log.i(this.javaClass.simpleName, message)
+fun Any.logInfo(message: String, tag: String = this.javaClass.simpleName) {
+    val _tag = if (tag.isEmpty()) "Unknown" else tag
+    Log.i(_tag, message)
 }
